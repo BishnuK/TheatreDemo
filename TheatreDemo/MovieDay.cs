f534a849edcfd978ddc1f2ec7bc5f4c7c0849eac
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-06 17:09:33 -0400

    Added Show class

---
+++ TheatreDemo/MovieDay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatreDemo
+{
+    enum MovieDay { Sun, Mon, Tue, Wed, Thu, Fri, Sat }
+    }
+
+
---
+++ TheatreDemo/Show.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatreDemo
+{
+    class Show
+    {
+        public double Price { get; private set; }
+        public MovieDay Day { get; private set; }
+        public Movie Movie { get; private set; }
+        public Time Time { get; private set; }
+        public Show(Movie movie, MovieDay day, double price, Time time)
+        {
+            Price = price;
+            Day = day;
+            Movie = movie;
+            Time = time;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2:c} {3}", Movie, Day, Price, Time);
+        }
+    }
+}
