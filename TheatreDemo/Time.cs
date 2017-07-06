53c59cf0ee5284f0d9b94d8a9774ab5b144d8094
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-06 16:50:51 -0400

    Added a Time class

--- TheatreDemo/Time.cs
+++ TheatreDemo/Time.cs
@@ -8,5 +8,19 @@ namespace TheatreDemo
 {
     class Time
     {
+
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+        public Time(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}:{2}", Hours, Minutes, Seconds);
+        }
     }
 }
